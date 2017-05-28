90c825f6b2c4fa140cad910c71fa07b2503094ce
richy_latham@yahoo.co.uk <richy_latham@yahoo.co.uk>
2017-05-28 17:31:23 +0100

    Inital version of reusable control
    An example project buitlt on create-react-app designed to show the use
    of creation and re-use of React components

---
+++ src/richyl.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+  const RichyL = React.createClass({
+  render() {
+    return (
+      <h1>RichyL</h1>
+    );
+  }
+});
+
+export   {RichyL}
+
+
+//------------------------------------------------------------------------------
